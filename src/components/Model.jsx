d74e9571f6ab50534f1e4ea1aaae0d732eb4db50
Max Doubin <95324785+MaxDoubin@users.noreply.github.com>
2026-08-25 01:29:10 -0700

    Keep the model animation timeline stable

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -31,21 +31,36 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
-  const tl = gsap.timeline();
+  const timeline = useRef(null);
+  if (!timeline.current) timeline.current = gsap.timeline();
 
   useEffect(() => {
     if (size === "large") {
-      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
-        transform: "translateX(-100%)",
-        duration: 2,
-      });
+      animateWithGsapTimeline(
+        timeline.current,
+        small,
+        smallRotation,
+        "#view1",
+        "#view2",
+        {
+          transform: "translateX(-100%)",
+          duration: 2,
+        },
+      );
     }
 
     if (size === "small") {
-      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
-        transform: "translateX(0)",
-        duration: 2,
-      });
+      animateWithGsapTimeline(
+        timeline.current,
+        large,
+        largeRotation,
+        "#view2",
+        "#view1",
+        {
+          transform: "translateX(0)",
+          duration: 2,
+        },
+      );
     }
   }, [size]);
 
